17317d672d7390ae103815d5992ba5bd9dbda57a
Harsha Kamakshigari <harsha.kamakshigari@gmail.com>
2025-07-18 23:20:48 +0530

    Learned vars in go

---
+++ CH-1/vars.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var name string = "im batman"
+
+	var age int8 = 21
+
+	var place = "Tpty"
+
+	mail := "batman@mail.com" //only work in func
+
+	const pi = 3.14
+
+	fmt.Println("name", name)
+	fmt.Println("age", age)
+	fmt.Println("place", place)
+	fmt.Println("mail", mail)
+
+}
